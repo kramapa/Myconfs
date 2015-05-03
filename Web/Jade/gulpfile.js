85e9152b8410313b7ab4aee94718843cc9c55a8a
kramapa <kramapa@gmail.com>
2015-05-03 13:43:56 +0200

    set all tabs to 4

--- Web/Jade/gulpfile.js
+++ Web/Jade/gulpfile.js
@@ -1,32 +1,32 @@
 var
 	gulp 			= require('gulp'),
 	rename			= require('gulp-rename'), 				// Rename files
-	sourcemaps		= require('gulp-sourcemaps'),				// Generate SourceMaps
+	sourcemaps		= require('gulp-sourcemaps'),			// Generate SourceMaps
 	less			= require('gulp-less'),					// Compile LESS to CSS
 	jade 			= require('gulp-jade'),					// Compile JADE to HTML
-	minifyCss		= require('gulp-minify-css'),				// Minify CSS
+	minifyCss		= require('gulp-minify-css'),			// Minify CSS
 	uglify			= require('gulp-uglify'),				// Minify/Obfuscation JS
 	concat 			= require('gulp-concat'),				// Concatenate several files
-	autoprefixer		= require('gulp-autoprefixer'),				// Autoprefixer
+	autoprefixer	= require('gulp-autoprefixer'),			// Autoprefixer
 	size 			= require('gulp-filesize'),				// File Size
 	plumber 		= require('gulp-plumber'), 				// Don't stop if errors
-	livereload 		= require('gulp-livereload');				// Livereload
+	livereload 		= require('gulp-livereload');			// Livereload
 
 // ==================   LESS TASK  ================
 gulp.task('less', function()
 {
-	return gulp.src('./site/mybootstrap/less/style.less')				// use style.less as input
-		.pipe(plumber()) 							// Don't stop if errors
+	return gulp.src('./site/mybootstrap/less/style.less')	// use style.less as input
+		.pipe(plumber()) 									// Don't stop if errors
 		.pipe(sourcemaps.init())
-		.pipe(less())								// Compile less to css
-		.pipe(autoprefixer({							// Autoprefixer
+		.pipe(less())										// Compile less to css
+		.pipe(autoprefixer({								// Autoprefixer
 			browsers: ['last 50 versions'],
 			cascade: true
 		}))
-		//.pipe(minifyCss())							// Minify generated CSS
+		//.pipe(minifyCss())								// Minify generated CSS
 		.pipe(sourcemaps.write('../maps'))					// Write the map file to ../maps
-		.pipe(gulp.dest('./site/html/css/'))					// Compile to css
-		//.pipe(size('*css'));							// [gulp] Size example.ccs: 265.32 kB
+		.pipe(gulp.dest('./site/html/css/'))				// Compile to css
+		//.pipe(size('*css'));								// [gulp] Size example.ccs: 265.32 kB
 		.pipe(livereload());
 });
 
